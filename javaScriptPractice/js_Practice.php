f2e66d9533c30668f92f1ce72169b9004d08fc0c
labib999 <labibeducation123@gmail.com>
2026-03-08 14:07:30 +0600

    java first

---
+++ javaScriptPractice/js_Practice.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JAVASCRIPT FILE</title>
+        <script>
+            var name = "MD Shariar Hossen Labib";
+            var age = 78;
+            var uni = "AIUB";
+            var is_human = true;
+
+            console.log(name);
+            console.log(age);
+            console.log(uni);
+            console.log(is_human);
+        </script>
+    </head>
+</html>
