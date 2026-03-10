e046f7679527750d99fb612a79f876ab16b994ff
labib999 <labibeducation123@gmail.com>
2026-03-10 14:45:59 +0600

    calculator lab task

---
+++ Calculator/script.js
@@ -0,0 +1,27 @@
+let display = document.getElementById("display")
+
+function press(value){
+
+    if(display.innerText === "0"){
+        display.innerText = value
+    }
+    else{
+        display.innerText += value
+    }
+
+}
+
+function clearDisplay(){
+    display.innerText = "0"
+}
+
+function calculate(){
+
+    try{
+        display.innerText = eval(display.innerText)
+    }
+    catch{
+        display.innerText = "Error"
+    }
+
+}
